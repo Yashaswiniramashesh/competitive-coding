35b0e521c4ebdec6ef922e0567d42bdc35148d10
YASHASWINI R <47792262+Yashaswiniramashesh@users.noreply.github.com>
2025-08-09 17:05:53 -0700

    Time: 0 ms (100.00%) | Memory: 19.8 MB (29.11%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -6,16 +6,16 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def Valid (root, left, right):
+        def dfs_Valid (root, left, right):
             if root is None:
                 return True
-            if (left < root.val < right) is False:
+            if (left < root.val < right) is False:# preorder processing
                 return False
             
-            left_flag = Valid(root.left, left, root.val) 
-            right_flag = Valid(root.right, root.val, right)
+            left_flag = dfs_Valid(root.left, left, root.val)  # traverse to left 
+            right_flag = dfs_Valid(root.right, root.val, right) # traverse to right
             return left_flag and right_flag
 
-        return Valid(root, float('-inf'), float('+inf'))
+        return dfs_Valid(root, float('-inf'), float('+inf'))
                 
         
